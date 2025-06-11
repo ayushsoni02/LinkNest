b2e93be25c5f842f1fc3220c79128fc4b064b88f
Ayush Soni <ayushsoni1021@gmail.com>
2025-06-11 17:52:40 +0530

    Add analytics

--- MindVault-FE/src/App.tsx
+++ MindVault-FE/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter,Route,Routes } from "react-router-dom"
 import Index from "./Pages/Index"
 import About from "./Pages/About"
 
+
 function App() {
 
   return <BrowserRouter>
@@ -16,6 +17,7 @@ function App() {
       <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>
+  
 }
 
 export default App
