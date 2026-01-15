dea61e91efab5594d04fd68e0623f96dc325a2d9
Ayush Soni <ayushsoni1021@gmail.com>
2026-01-15 10:42:50 +0530

    fix the env error

--- MindVault-FE/src/Config.tsx
+++ MindVault-FE/src/Config.tsx
@@ -1,4 +1,4 @@
-export const BACKEND_URL= "http://localhost:3000";
-export const FRONTEND_URL = "http://localhost:5173";
-// export const  BACKEND_URL = "https://cerebra.onrender.com"
-// export const FRONTEND_URL = "https://cerebra-gamma.vercel.app"
+// export const BACKEND_URL= "http://localhost:3000";
+// export const FRONTEND_URL = "http://localhost:5173";
+export const  BACKEND_URL = "https://cerebra.onrender.com"
+export const FRONTEND_URL = "https://cerebra-gamma.vercel.app"
